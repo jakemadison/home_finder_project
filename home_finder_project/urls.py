d97dddd7644bbce6bd0305b1d7f17e40f8a929e5
Jake Madison <jakemadison@gmail.com>
2014-12-09 16:41:12 -0800

    wrapping head around django quirks.

--- home_finder_project/urls.py
+++ home_finder_project/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^craig_data_getter/', include('craig_data_getter.urls')),
 )
