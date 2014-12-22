6f529ac164a5847a9b75fb05c99a11a4f259c4af
Jake Madison <jakemadison@gmail.com>
2014-12-22 09:04:49 -0800

    much more consumable format for now on the new index page.

--- static/js/main.js
+++ static/js/main.js
@@ -5,34 +5,15 @@
 
 console.log("main.js is running now...");
 
-//$(window).load(function () {
-//        $('.Collage').collagePlus();
-//        console.log("collagiying now....");
-//    });
-
-
-
 
 $(".Collage").justifiedGallery({
     randomize: true,
     margins: 2,
     rowHeight: 200,
     //lastRow : 'hide',
-    fixedHeight: true
+    captions: false
+    //fixedHeight: true
 
 }).on('jg.complete', function (e) {
     console.log('on complete');
 });
-//
-//
-//console.log("am I doing something? Anything??");
-//$("#basic2").justifiedGallery({
-//    randomize: true,
-//    margins: 2,
-//    rowHeight: 400,
-//    lastRow : 'justify',
-//    fixedHeight: true
-//
-//}).on('jg.complete', function (e) {
-//    console.log('on complete');
-//});
