ffec34d67b3b21c31dea517c32d7fd655ba84300
Jake Madison <jakemadison@gmail.com>
2014-12-27 11:18:24 -0800

    inter-app communication works.

--- datachecker/views.py
+++ datachecker/views.py
@@ -4,10 +4,16 @@ from django.shortcuts import render_to_response
 # Create your views here.
 from django.http import HttpResponse
 # Create your views here.
+from datagetter.models import Postings
 
 
 def index(request):
     context = RequestContext(request)
-    context_dict = {'boldmessage': "Craigs Data Grabber will go here"}
+    post = Postings.objects.all()[:1]
+
+    print post
+    print dir(post)
+
+    context_dict = {'boldmessage': "Craigs Data Grabber will go here", 'post': post}
 
     return render_to_response('datachecker/main.html', context_dict, context)
