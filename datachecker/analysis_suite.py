18c63a52c17b94612785f434d31e1718424221d1
Jake Madison <jakemadison@gmail.com>
2014-12-27 13:29:57 -0800

    oh dear god, scipy FINALLY installed on the virtualenv.

---
+++ datachecker/analysis_suite.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+from sklearn.ensemble import RandomForestClassifier
+from numpy import genfromtxt, savetxt
+
+
+def random_forrest():
+    print('i"m in a random forrest!')
+
+    return
+
+
+if __name__ == "__main__":
+    random_forrest()
--- home_finder_project/settings.py
+++ home_finder_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'home_finder',
     'datagetter',
     'djangular',
+    'datachecker',
 )
 
 MIDDLEWARE_CLASSES = (
